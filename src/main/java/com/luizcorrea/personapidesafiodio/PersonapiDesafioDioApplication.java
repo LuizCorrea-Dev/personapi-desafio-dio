65421596ee4e362de4b6680ae4a6bbf28c3445f1
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2021-09-07 16:02:15 -0300

    Initial commit

---
+++ src/main/java/com/luizcorrea/personapidesafiodio/PersonapiDesafioDioApplication.java
@@ -0,0 +1,13 @@
+package com.luizcorrea.personapidesafiodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiDesafioDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiDesafioDioApplication.class, args);
+	}
+
+}
